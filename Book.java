9a981c1f8a39bed7b7316693b2ec6687b2d9180e
Bellloq <nrshirke@gmail.com>
2024-12-20 00:29:20 -0500

    Created new field (pages) and PrintDetails Method

--- Book.java
+++ Book.java
@@ -11,24 +11,31 @@ class Book
     // The fields.
     private String author;
     private String title;
-
+    private int pages;
     /**
      * Set the author and title fields when this object
      * is constructed.
      */
-    public Book(String bookAuthor, String bookTitle)
+    public Book(String bookAuthor, String bookTitle, int numPages)
     {
         author = bookAuthor;
         title = bookTitle;
+        pages = numPages;
     }
     public String getAuthor() { return author; }
     public String getTitle() { return title; } 
+    public int getPages () { return pages; } 
     
     public void printAuthor() {
         System.out.println(author);
     }
     public void printTitle() {
         System.out.println(title);
+    }
+    public void printDetails() {
+        System.out.println("Title: " + title);
+        System.out.println("Author: " + author);
+        System.out.println("Pages: " + pages);
     }
     // Add the methods here ...
 }
