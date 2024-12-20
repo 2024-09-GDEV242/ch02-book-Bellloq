f423f3f96cf424c0c2d2faa6e124e99641ca3f93
Bellloq <nrshirke@gmail.com>
2024-12-20 00:50:59 -0500

    Wrapped up by adding borrow and courseText fields

--- Book.java
+++ Book.java
@@ -13,27 +13,30 @@ class Book
     private String title;
     private int pages;
     private String refNumber;
+    private int borrowed;
+    private boolean courseText;
     /**
      * Set the author and title fields when this object
      * is constructed.
      */
-    public Book(String bookAuthor, String bookTitle, int numPages)
+    public Book(String bookAuthor, String bookTitle, int numPages, boolean cText)
     {
         author = bookAuthor;
         title = bookTitle;
         pages = numPages;
         refNumber = "";
+        courseText = cText;
     }
 
     public String getAuthor() { return author; }
     public String getTitle() { return title; } 
     public int getPages () { return pages; } 
     public String getRefNumber() { return refNumber; }
+    public int getBorrowed() {return borrowed;}
+    public boolean isCourseTest() {return courseText; }
     
-    public void setRefNumber(String def) { 
-        if(def.length() > 3) refNumber = def;
-    }
-    
+    public void setRefNumber(String def) { if(def.length() > 3) refNumber = def;}
+    public void borrow() {borrowed += 1;}
     
     public void printAuthor() {
         System.out.println(author);
@@ -47,6 +50,8 @@ class Book
         System.out.println("Pages: " + pages);
         if(refNumber.length() > 0) System.out.println("Reference Number: " + refNumber);
         else System.out.println("Reference Number: ZZZ");
+        System.out.println("Number of Times Borrowed: " + borrowed);
+        if(courseText) System.out.println("Is Course Text");
     }
     // Add the methods here ...
 }
